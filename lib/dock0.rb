7df3f905d1451cced48c77c3e77a5e67525e540d
Les Aker <me@lesaker.org>
2014-01-26 17:17:57 -0500

    finalize install

--- lib/dock0.rb
+++ lib/dock0.rb
@@ -35,25 +35,30 @@ module Dock0
       puts "Mounting filesystem on #{@config['paths']['mount']}"
       FileUtils.mkdir_p @config['paths']['mount']
       `mount #{@config['paths']['device']} #{@config['paths']['mount']} 2>&1`
+      puts "Making build FS at #{@config['paths']['build_file']}"
+      `dd if=/dev/zero of=#{@config['paths']['build_file']} bs=1M count=#{@config['root_size']} 2>&1`
+      `mkfs.ext2 -f #{@config['paths']['build_file']} 2>&1`
+      puts "Mounting FS at #{@config['paths']['build']}"
+      `mount #{@config['paths']['build_file']} #{@config['paths']['build']} 2>&1`
     end
 
     def install_packages
       File.read(@config['paths']['package_list']).split("\n").each do |package|
         puts "Installing #{package}"
-        `pacstrap -G -M #{@config['paths']['mount']} #{package} 2>&1`
+        `pacstrap -G -M #{@config['paths']['build']} #{package} 2>&1`
       end
     end
 
     def apply_overlay
       puts "Applying overlay from #{@config['paths']['overlay']}"
       overlay_path = @config['paths']['overlay'] + '/.'
-      FileUtils.cp_r overlay_path, @config['paths']['mount']
+      FileUtils.cp_r overlay_path, @config['paths']['build']
     end
 
     def run_scripts
       Dir.glob(@config['paths']['scripts'] + '/*.rb').each do |script|
         puts "Running #{script}"
-        load script
+        instance_eval File.read(script), script, 0
       end
     end
 
@@ -61,12 +66,20 @@ module Dock0
       cmds = @config['commands']
       cmds.fetch('chroot', []).each do |cmd|
         puts "Running #{cmd} in chroot"
-        `arch_chroot #{@config['path']['mount']} #{cmd} 2>&1`
+        `arch_chroot #{@config['path']['build']} #{cmd} 2>&1`
       end
       cmds.fetch('host', []).each do |cmd|
         puts "Running #{cmd} on host"
         `#{cmd} 2>&1`
       end
+    end
+
+    def finalize
+      puts "Packing up root FS"
+      `umount #{@config['paths']['build']} 2>&1`
+      `mksquashfs #{@config['paths']['build_file']} #{@config['paths']['mount']}/root.fs.sfs 2>&1`
+      puts "Unmounting target device"
+      `umount #{@config['paths']['mount']}`
     end
 
     def easy_mode
@@ -75,6 +88,7 @@ module Dock0
       apply_overlay
       run_scripts
       run_commands
+      finalize
     end
   end
 end
