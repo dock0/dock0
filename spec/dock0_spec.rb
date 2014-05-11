49b140d3870ceb92cfacb27d96f21881871f52c1
Les Aker <me@lesaker.org>
2014-05-11 19:59:29 -0400

    run tests

--- spec/dock0_spec.rb
+++ spec/dock0_spec.rb
@@ -8,15 +8,26 @@ describe Dock0 do
   end
 
   describe Dock0::Image do
+    let(:image) { Dock0::Image.new 'spec/examples/image.yml' }
+
     it 'loads stacked config files' do
-      image = Dock0::Image.new(
+      config_image = Dock0::Image.new(
         'spec/examples/alpha.yml', 'spec/examples/beta.yml'
       )
       result = { 'foo' => 'override', 'test' => 5, 'other' => 6 }
-      expect(image.config).to eql result
+      expect(config_image.config).to eql Dock0::DEFAULT_CONFIG.merge(result)
     end
     it 'has a timestamp' do
       expect(Dock0::Image.new.stamp).to match(/\d{4}-\d{2}-\d{2}-\d{4}/)
+    end
+
+    describe '#run' do
+      it 'runs a command' do
+        expect(image.run('echo "hello"')).to eql 'hello'
+      end
+      it 'raises if the command fails' do
+        expect { image.run('dasdks') }.to raise_error RuntimeError
+      end
     end
   end
 end
